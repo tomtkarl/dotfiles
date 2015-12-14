7b7f8cfda6af1a71c04ac8a82764ee61e8d0cf89
Karl Barker <kbarker19@bloomberg.net>
2015-12-14 09:14:46 -0500

    add tmux conf

--- setup.sh
+++ setup.sh
@@ -8,17 +8,18 @@
 	.vimrc \
 	.Xresources \
     .inputrc \
-    .ackrc
+    .ackrc \
+    .tmux.conf
 do
-	echo "Linking ${df}"
+	tput setaf 5; echo "Linking ${df}"; tput sgr0
 	if [[ -f ~/${df} ]]
 	then
-		echo "Backing up existing ~/${df}"
+		tput setaf 1; echo "Backing up existing ~/${df}"; tput sgr0
 		mv ~/${df} ~/${df}.bak
 	fi
 	ln -s ~/.dotfiles/${df} ~/${df}
 done
-if [[! -d ~/.vim/bundle/Vundle.vim ]]; then \
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then \
     #clone vundle for vim
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 fi
